fb73b02b0893710b5aed9e6b0ff926c89cd23930
itsgosho <itsgosho2@gmail.com>
2019-05-02 22:55:12 +0300

    Pull method done
    From Heap  & PQ java

--- Data Structures/#08.Heaps & Priority Queue Lab/Java/src/main/java/BinaryHeap.java
+++ Data Structures/#08.Heaps & Priority Queue Lab/Java/src/main/java/BinaryHeap.java
@@ -31,9 +31,9 @@ public class BinaryHeap<T extends Comparable<T>> {
         int currentIndex = heap.size() - 1;
         T currentElement = heap.get(heap.size() - 1);
 
-        while (parentIndexElement.compareTo(currentElement) < 0){
-            this.heap.set(parentIndex,currentElement);
-            this.heap.set(currentIndex,parentIndexElement);
+        while (parentIndexElement.compareTo(currentElement) < 0) {
+            this.heap.set(parentIndex, currentElement);
+            this.heap.set(currentIndex, parentIndexElement);
 
             currentIndex = parentIndex;
             parentIndex = (parentIndex - 1) / 2;
@@ -42,9 +42,31 @@ public class BinaryHeap<T extends Comparable<T>> {
 
     }
 
+    private void heapifyDown() {
+        int elementIndex = 0;
+        T element = this.heap.get(elementIndex);
+        int elementLeftChildIndex = 2 * elementIndex + 1;
+        T elementLeftChild = this.heap.get(elementLeftChildIndex);
+
+        while (elementLeftChild != null &&  element.compareTo(elementLeftChild) < 0) {
+            this.heap.set(elementIndex, elementLeftChild);
+            this.heap.set(elementLeftChildIndex, element);
+
+            elementIndex = elementLeftChildIndex;
+            elementLeftChildIndex = 2 * elementIndex + 1;
+
+            if (elementLeftChildIndex <= this.heap.size() - 1) {
+                elementLeftChild = this.heap.get(elementLeftChildIndex);
+            } else {
+                break;
+            }
+        }
+
+    }
+
     public T peek() {
 
-        if(this.size == 0){
+        if (this.size == 0) {
             throw new IllegalArgumentException();
         }
 
@@ -52,6 +74,23 @@ public class BinaryHeap<T extends Comparable<T>> {
     }
 
     public T pull() {
-        throw new UnsupportedOperationException();
+
+        if (this.size == 0) {
+            throw new IllegalArgumentException();
+        }
+
+
+        T element = this.heap.get(0);
+
+        this.heap.set(0, this.heap.get(this.size - 1));
+        this.heap.set(this.size -1 , null);
+
+        this.size--;
+        if(this.size > 1){
+            this.heapifyDown();
+        }
+
+        return element;
+        //TODO: ako e samo edin da go opraq!!!
     }
 }
--- Data Structures/#08.Heaps & Priority Queue Lab/Java/src/main/java/Main.java
+++ Data Structures/#08.Heaps & Priority Queue Lab/Java/src/main/java/Main.java
@@ -4,17 +4,12 @@ public class Main {
 
         //WILL BE MAX HEAP !!!
         BinaryHeap<Integer> binaryHeap = new BinaryHeap<>();
-        binaryHeap.insert(10);
         binaryHeap.insert(3);
-        binaryHeap.insert(15);
-        binaryHeap.insert(25);
-        binaryHeap.insert(4);
+        binaryHeap.insert(5);
+        binaryHeap.insert(6);
         binaryHeap.insert(7);
-        binaryHeap.insert(8);
-        binaryHeap.insert(43);
-        binaryHeap.insert(9);
 
 
-        System.out.println(binaryHeap.peek());
+        System.out.println(binaryHeap.pull());
     }
 }
