9ed0a1580ec38f44233f6ea979b65f82379a4418
itsgosho <itsgosho2@gmail.com>
2021-02-11 01:42:31 +0200

    Uploaded the quadratic equation solution

--- C++/C++ Fundamentals/#01.Introduction to C++ and Basic Syntax/Solutions/3.quadratic-equation.cpp
+++ C++/C++ Fundamentals/#01.Introduction to C++ and Basic Syntax/Solutions/3.quadratic-equation.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 int main()
 {
-    std::string consoleInput = "-1";
+    std::string aConsoleInput = "";
+    std::string bConsoleInput = "";
+    std::string cConsoleInput = "";
 
-    int test = std::stoi(consoleInput);
-    std::cout << test;
+    std::cin >> aConsoleInput;
+    std::cin >> bConsoleInput;
+    std::cin >> cConsoleInput;
+
+    float a = std::stof(aConsoleInput);
+    float b = std::stof(bConsoleInput);
+    float c = std::stof(cConsoleInput);
+
+    float discriminant = std::pow(b, 2) - 4 * a * c;
+
+    if (discriminant < 0)
+    {
+        std::cout << "no roots";
+        return 0;
+    }
+
+    float x1 = (-b + sqrt(discriminant)) / (2 * a);
+    float x2 = (-b - sqrt(discriminant)) / (2 * a);
+
+    if (x1 != x2)
+        std::cout << x1 << " " << x2;
+    else
+        std::cout << x1;
 }
