dbfad3421b26b07eeb610d4a9215d5c96e8a4d2e
itsgosho <itsgosho2@gmail.com>
2021-03-08 00:57:24 +0200

    Uploaded the solutions for the memory management

---
+++ C++/C++ Advanced/#02.Memory Management/Solutions/#05.Serialize/cmake-build-debug/CMakeFiles/_05_Serialize.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Gosho/Desktop/GitHub/SoftUni Open/C++/C++ Advanced/#02.Memory Management/Solutions/#05.Serialize/main.cpp" "C:/Users/Gosho/Desktop/GitHub/SoftUni Open/C++/C++ Advanced/#02.Memory Management/Solutions/#05.Serialize/cmake-build-debug/CMakeFiles/_05_Serialize.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
