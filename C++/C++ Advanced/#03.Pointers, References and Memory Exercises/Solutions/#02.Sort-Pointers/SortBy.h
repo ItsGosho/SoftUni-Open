f34d5ae4fa493663631f11abc572a74e0a8da785
itsgosho <itsgosho2@gmail.com>
2021-03-08 11:53:31 +0200

    Refactored the solution for sorting the pointers

--- C++/C++ Advanced/#03.Pointers, References and Memory Exercises/Solutions/#02.Sort-Pointers/SortBy.h
+++ C++/C++ Advanced/#03.Pointers, References and Memory Exercises/Solutions/#02.Sort-Pointers/SortBy.h
@@ -1,6 +1,12 @@
 #include "Company.h"
 #include "algorithm"
 
+void swapPointerValues(Company* a, Company* b) {
+    Company firstTemp = *a;
+    *a = *b;
+    *b = firstTemp;
+}
+
 void sortBy(Company **begin, Company **end, bool (*sortPredicate)(const Company &, const Company &)) {
 
     Company **beginCopy = begin;
@@ -15,16 +21,15 @@ void sortBy(Company **begin, Company **end, bool (*sortPredicate)(const Company 
 
         begin = beginCopy;
         endCopy = endCopy;
+
         while (begin + 1 != end) {
             Company *first = *begin;
             Company *second = *(begin + 1);
 
-            bool swapRequired = sortPredicate(*first, *second);
+            bool swapRequired = !sortPredicate(*first, *second);
 
-            if (!swapRequired) {
-                Company firstTemp = *first;
-                *first = *second;
-                *second = firstTemp;
+            if (swapRequired) {
+                swapPointerValues(first, second);
                 isSwapMade = true;
             }
 
